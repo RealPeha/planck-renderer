a89f672cb5320f239317cf2fb50ae462da7b7602
RealPeha <real.peha@gmail.com>
2019-09-05 16:25:33 +0300

    custom render, more renderer settings, more accurate rendering

--- README.md
+++ README.md
@@ -29,8 +29,13 @@ const world = new World(Vec2(0, -10));
 const renderer = new Renderer(world, ctx, {
 	// default settings
 	fps: 60,
-	scale: 1,
+	scale: 16,
 	speed: 1,
+	strokeStyle: {
+    	dynamic: 'black',
+    	static: 'black',
+    	kinematic: 'black',
+    },
 })
 
 // init world entities
@@ -42,3 +47,78 @@ renderer.startUpdate(); // start rendering world
 ```
 
 A more detailed example can be found in the folder [example](https://github.com/RealPeha/planck-renderer/tree/master/example "example")
+
+### renderer.draw
+
+If you need to draw something on the canvas in addition to the physical objects of the engine, then you can do this in the renderer method.
+This method returns context and current fps.
+
+```javascript
+
+renderer.draw = (ctx, fps) => {
+	ctx.strokeText(`FPS: ${fps}`0, 0)
+}
+```
+
+### renderer.update
+
+if you need to do any data updates to the beat of the main loop do it in the renderer.update method
+
+```javascript
+
+renderer.update = (step) => {
+	// something...
+}
+```
+
+### Custom figure drawing
+
+```javascript
+
+const ball = world.createDynamicBody(Vec2(5.0, -30.0));
+ball.createFixture(Circle(3.0), {
+	density: 5.0,
+});
+ball.render = {
+	stroke: 'tomato', // stroke style only for this body
+	// or custom drawing function
+	custom: (ctx, pos, size) => {
+		// draw your circle
+	}
+}
+```
+
+### Custom render function
+
+the first argument this function always returns context. For the circle, the next two arguments will be position (object - x, y) and size (number). For a polygon, the next two arguments will be position (object - x, y) and size (object - width, height)
+
+Let's draw a ball texture instead of a circle
+
+```javascript
+
+const renderer = new Renderer(world, ctx)
+
+const init = img => {
+	const ball = world.createDynamicBody(Vec2(5.0, -30.0));
+	ball.createFixture(Circle(3.0), {
+		density: 5.0,
+	});
+	ball.render = {
+		stroke: 'tomato',
+		custom: (ctx, pos, size) => {
+			ctx.drawImage(ballImg, pos.x, pos.y, size, size)
+			return true // optional
+		}
+	}
+
+	renderer.startUpdate()
+}
+
+const img = new Image()
+img.src = "https://pngriver.com/wp-content/uploads/2018/04/Download-Swimming-Pool-Ball-PNG-File.png"
+img.onload = () => {
+	init(img)
+}
+```
+
+if the custom function returns true, then it draws the custom method and the built-in
--- example/Revolute.js
+++ example/Revolute.js
@@ -13,14 +13,13 @@ canvas.height = window.innerHeight
 const ctx = canvas.getContext('2d')
 
 ctx.translate(canvas.width / 2, canvas.height / 2)
-ctx.scale(-1, 1)
-ctx.rotate(Math.PI)
 
+const world = new World(Vec2(0, 10));
 
-const world = new World(Vec2(0, -10));
+const scale = 10
 
 const renderer = new Renderer(world, ctx, {
-  scale: 10,
+  scale: scale,
 })
 
 renderer.clear = (canvas, ctx) => {
@@ -32,7 +31,12 @@ renderer.clear = (canvas, ctx) => {
   );
 }
 
-const setup = () => {
+// Pixels to meters
+const toM = num => num / scale
+// Meters to pixels
+const toPX = num => num * scale
+
+const setup = img => {
   const ground = world.createBody();
 
   const groundFD = {
@@ -40,9 +44,9 @@ const setup = () => {
     filterMaskBits: 0xFFFF,
     filterGroupIndex: 0,
   };
-  ground.createFixture(Edge(Vec2(-40.0, 0.0), Vec2(40.0, 0.0)), groundFD);
+  ground.createFixture(Edge(Vec2(-40, 0.0), Vec2(40, 0.0)), groundFD);
 
-  const rotator = world.createDynamicBody(Vec2(-10.0, 20.0));
+  const rotator = world.createDynamicBody(Vec2(-10.0, -20.0));
   rotator.createFixture(Circle(0.5), 5.0);
 
   const w = 100.0;
@@ -57,35 +61,49 @@ const setup = () => {
     upperAngle: 0.5 * Math.PI,
     enableLimit: false,
     collideConnected: true,
-  }, ground, rotator, Vec2(-10.0, 12.0)));
+  }, ground, rotator, Vec2(-10.0, -12.0)));
 
-  const ball = world.createDynamicBody(Vec2(5.0, 30.0));
+  const ball = world.createDynamicBody(Vec2(5.0, -30.0));
   ball.createFixture(Circle(3.0), {
     density: 5.0,
   });
+  ball.render = {
+    custom: (ctx, pos, size) => {
+      ctx.drawImage(img, pos.x, pos.y, size, size)
+      return true
+    }
+  }
 
   const platform = world.createBody({
-    position: Vec2(20.0, 10.0),
+    position: Vec2(20.0, -10.0),
     type: 'dynamic',
     bullet: true,
   });
-  platform.createFixture(Box(10.0, 0.2, Vec2(-10.0, 0.0), 0.0), 2.0);
+  platform.createFixture(Box(10, 0.2, Vec2(-10.0, 0.0), 0.0), 2.0);
 
   world.createJoint(RevoluteJoint({
-    lowerAngle: -0.25 * Math.PI,
-    upperAngle: 0.0 * Math.PI,
+    lowerAngle: 0.0 * Math.PI,
+    upperAngle: 0.25 * Math.PI,
     enableLimit: true,
-  }, ground, platform, Vec2(20.0, 10.0)));
+  }, ground, platform, Vec2(20.0, -10.0)));
 
   const triangle = world.createDynamicBody();
-
   triangle.createFixture(Polygon([
-    Vec2(17.63, 36.31),
-    Vec2(17.52, 36.69),
-    Vec2(17.19, 36.36)
+    Vec2(17.63, -36.31),
+    Vec2(17.52, -36.69),
+    Vec2(17.19, -36.36)
   ]), 1);
+
+  renderer.draw = (ctx, fps) => {
+    ctx.fillText(`FPS: ${fps}`, 0, 0)
+  }
 }
 
-setup()
+const img = new Image()
+img.src = "https://pngriver.com/wp-content/uploads/2018/04/Download-Swimming-Pool-Ball-PNG-File.png"
+img.onload = () => {
+  setup(img)
+}
+
 
 renderer.startUpdate();
